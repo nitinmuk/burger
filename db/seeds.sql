ac5b3c3d5bc67b44882b2d90825625c0516365f7
Nitin Mukesh <nitinmukes@gmail.com>
2020-08-09 02:00:58 +0800

    Updated sample data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers(burger_name,devoured) VALUES("breakfast burger",'0');
+INSERT INTO burgers(burger_name,devoured) VALUES("breakfast burger",'1');
 INSERT INTO burgers(burger_name,devoured) VALUES("lunch burger",'0');
-INSERT INTO burgers(burger_name,devoured) VALUES("dinner burger",'1');
+INSERT INTO burgers(burger_name,devoured) VALUES("dinner burger",'0');
