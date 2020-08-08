664db529971bdb5a06328a001016524c96080429
Nitin Mukesh <nitinmukes@gmail.com>
2020-08-08 23:06:07 +0800

    Change DB column name to lowercase and update sample data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers(BURGER_NAME,DEVOURED) VALUES("breakfast burger",'0');
-INSERT INTO burgers(BURGER_NAME,DEVOURED) VALUES("lunch burger",'0');
-INSERT INTO burgers(BURGER_NAME,DEVOURED) VALUES("dinner burger",'0');
+INSERT INTO burgers(burger_name,devoured) VALUES("breakfast burger",'0');
+INSERT INTO burgers(burger_name,devoured) VALUES("lunch burger",'0');
+INSERT INTO burgers(burger_name,devoured) VALUES("dinner burger",'1');
