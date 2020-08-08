05e2d8e0d125fb87b2c63ac682f17027c4002889
Nitin Mukesh <nitinmukes@gmail.com>
2020-08-09 01:59:22 +0800

    Added needed routes

--- controllers/burgersControllers.js
+++ controllers/burgersControllers.js
@@ -4,7 +4,44 @@ const burger = require("../models/burger.js");
 const router = express.Router();
 
 router.get("/", (request, response) => {
-    response.status(200).json();
+    burger
+        .all()
+        .then(data => {
+            const hbsObject = {
+                burgers: data
+            };
+            response.render("index", hbsObject);
+        })
+        .catch(error => {
+            console.log(error);
+            response.status(500).end();
+        });
+});
+
+router.post("/burgers", (request, response) => {
+    burger
+        .create(request.body)
+        .then(result => response.json({ id: result.insertId }))
+        .catch(error => {
+            console.log(error);
+            response.status(500).end();
+        });
+});
+
+router.put("/burgers/:id", (request, response) => {
+    burger
+        .update({
+            burger: request.body,
+            where: {
+                id: request.params.id
+            }
+        })
+        .then(result => response.status(result.changedRows == 0 ? 404 : 204).end())
+        .catch(error => {
+            console.log(error);
+            response.status(500).end();
+        });
+
 });
 
 
