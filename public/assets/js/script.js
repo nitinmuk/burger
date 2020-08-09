18ad58b66a497c87bb8788f0d805e7e64db5d972
Nitin Mukesh <nitinmukes@gmail.com>
2020-08-09 16:25:18 +0800

    Added alert modal if user try to submit without any burger name

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,22 @@
 $(document).ready(() => {
     $(document).on("click", ".burger-submit-btn", (event) => {
         event.preventDefault();
-        const newBurger = {
-            burger_name: $("#burger-name").val().trim(),
-            devoured: 0
-        };
-        $.ajax("/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(() => location.reload())
-            .catch(error => console.log(error));
+        const providedBurgerName = $("#burger-name").val().trim();
+        if (providedBurgerName && providedBurgerName.length) {
+            const newBurger = {
+                burger_name: providedBurgerName,
+                devoured: 0
+            };
+            $.ajax("/burgers", {
+                type: "POST",
+                data: newBurger
+            }).then(() => location.reload())
+                .catch(error => console.log(error));
+        } else {
+            $("#burger-status-header").text("Burger Name is Missing");
+            $("#burger-status-content").text("Please enter a valid burger name.");
+        }
+        $(".create-form").trigger("reset");
     });
 
     $(document).on("click", ".devour-button", (event) => {
